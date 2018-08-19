a9e6aad7ae27ef632a19a01ce9386c44704440b5
Francis DiTraglia <fditra@sas.upenn.edu>
2018-08-19 16:55:43 -0400

    finish ps02 and lab04

--- ps/ps02-STAR-solutions.R
+++ ps/ps02-STAR-solutions.R
@@ -22,8 +22,9 @@ star %>%
   summarize(avg_math = mean(g4math, na.rm = TRUE),
             sd_math = sd(g4math, na.rm = TRUE),
             avg_read = mean(g4reading, na.rm = TRUE),
-            sd_read = sd(g4reading, na.rm = TRUE),
-            p_grad = mean(hsgrad, na.rm = TRUE))
+            sd_read = sd(g4reading, na.rm = TRUE))
+
+
 
 test_by_small <- function(varname) {
   x_small <- star %>% 
@@ -37,7 +38,6 @@ test_by_small <- function(varname) {
 
 test_by_kindersmall('g4math')
 test_by_kindersmall('g4reading')
-test_by_kindersmall('hsgrad')
 
 # Question 3
 star %>%
@@ -55,17 +55,58 @@ star %>%
 star %>% 
   group_by(yearssmall) %>%
   summarize(avg_math = mean(g4math, na.rm = TRUE),
-            avg_read = mean(g4reading, na.rm = TRUE),
-            p_grad = mean(hsgrad, na.rm = TRUE))
+            avg_read = mean(g4reading, na.rm = TRUE))
 
 ggplot(star) +
-  geom_boxplot(aes(x = factor(yearssmall), y = g4reading))
-  
+  geom_boxplot(aes(x = factor(yearssmall), y = g4reading)) +
+  ggtitle('Grade 4 Reading Score by Years in Small Class') + 
+  xlab('Years') + ylab('Reading Score')
+
+ggplot(star) +
+  geom_boxplot(aes(x = factor(yearssmall), y = g4math)) +
+  ggtitle('Grade 4 Math Score by Years in Small Class') + 
+  xlab('Years') + ylab('Math Score')
+
+# Test those who spent 4 years in small classes against those who spent 4 in regular
+test_by_4small <- function(varname) {
+  x_4small <- star %>%
+    filter(yearssmall == 4) %>%
+    pull(varname)
+  x_4regular <- star %>%
+    filter(yearssmall == 0) %>%
+    pull(varname)
+  t.test(x_4small, x_4regular)
+}
+test_by_4small('g4reading')
+test_by_4small('g4math')
 
 
+# Question 5
+star <- star %>%
+  mutate(minority = (race == 'black' | race == 'hispanic'))
 
+star %>%
+  group_by(kinder, minority) %>%
+  summarize(avg_math = mean(g4math, na.rm = TRUE),
+            avg_read = mean(g4reading, na.rm = TRUE))
 
+test_by_minority <- function(varname) {
+  x_minority <- star %>%
+    filter(minority == TRUE) %>%
+    pull(varname)
+  x_white <- star %>%
+    filter(minority == FALSE) %>%
+    pull(varname)
+  t.test(x_minority, x_white)
+}
 
+test_by_minority('g4reading')
+test_by_minority('g4math')
+
+# Question 6
+test_by_kindersmall('hsgrad')
+test_by_4small('hsgrad')
+test_by_minority('hsgrad')
 
 
 
