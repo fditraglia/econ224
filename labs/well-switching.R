f34a10c299f1ed7d4cfeac7440f47aa2b1d0566a
Francis DiTraglia <fditra@sas.upenn.edu>
2018-09-21 17:42:56 -0400

    work on lab09

--- labs/well-switching.R
+++ labs/well-switching.R
@@ -14,7 +14,7 @@ wells <- read_csv('~/econ224/labs/wells.csv')
 # assoc = 1 if any members of household are active in community organizations
 # educ = education level of head of household (in years)
 
-# Histogram of arsenic levels in the unsafe wells
+#---------------------- Histogram of arsenic levels in the unsafe wells
 unsafe <- wells %>%
   filter(arsenic > 0.5)
 ggplot(unsafe) + 
@@ -22,20 +22,23 @@ ggplot(unsafe) +
   xlab('Arsenic Concentration (100s of micrograms/liter)') + 
   ggtitle('Arsenic Concentrations in Unsafe Wells')
 
+#----------------------- Logit regression using only distance to safe well
 fit1 <- glm(switch ~ dist, family = binomial(link = 'logit'), wells)
 summary(fit1)
 
-# change distance to 100s of meters
+#----------------------- Change distance to 100s of meters
 wells <- wells %>%
   mutate(dist100 = dist / 100)
 
 fit2 <- glm(switch ~ dist100, family = binomial(link = 'logit'), wells)
 summary(fit2)
 
+#----------------------- Plot histogram of dist100
 ggplot(wells) + 
   geom_histogram(aes(x = dist100)) + 
   xlab('Distance to Nearest Safe Well (100s of Meters)')
 
+#----------------------- Plot logistic with jittering
 ggplot(wells, aes(x = dist100, y = switch)) +
   geom_jitter(height = 0.1) +
   stat_smooth(method='glm', 
@@ -44,10 +47,10 @@ ggplot(wells, aes(x = dist100, y = switch)) +
   xlab('Distance to Nearest Safe Well (100s of meters)') +
   ylab("P(Switch)")
 
-# What is the probability of swtiching if you live right next to a safe well?
+#----------------------- Prob(switch) if dist100 = 0
 # Using predict:
-predict(fit2, newdata = data.frame(dist = 0), type = 'response')
-# What happens if you don't specify response?
+predict(fit2, newdata = data.frame(dist100 = 0), type = 'response')
+# What happens if you don't specify type = 'response'
 
 # By hand:
 Lambda <- function(x) {
@@ -55,16 +58,27 @@ Lambda <- function(x) {
 }
 Lambda(0.60596)
 
-# Derivative at the *average* value of dist in the dataset
+#---------------------- Prob(switch) at the avg of dist100
 avgdist <- wells %>% 
   summarize(avgdist = mean(dist100)) %>% 
   pull(avgdist)
+avgdist
+# Using predict
+predict(fit2, newdata = data.frame(dist100 = avgdist), type = 'response')
+# By hand
+Lambda(coef(fit2)[1] + coef(fit2)[2] * avgdist)
 
-# By hand:
-# Need to actually compute the derivative here!
+
+#------------------------ Marginal effect of dist100 at the average
+b0 <- coef(fit2)[1]
+b1 <- coef(fit2)[2]
+
+linear_predictor <- b0 + b1 * avgdist
+
+b1 / ((1 + exp(-1 * linear_predictor))^2)
 
 # Divide by 4 rule
--0.62 / 4
+avgdist / 4
 
 # Confidence interval for slope
 
